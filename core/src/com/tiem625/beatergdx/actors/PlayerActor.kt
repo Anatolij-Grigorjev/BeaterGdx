5ae386158555487db9ac6cbecafb251a20f20485
Anatolij Grigorjev <anatolij@mediapark.lt>
2018-04-22 01:25:19 +0300

    add code that tests user input response

--- core/src/com/tiem625/beatergdx/actors/PlayerActor.kt
+++ core/src/com/tiem625/beatergdx/actors/PlayerActor.kt
@@ -5,11 +5,13 @@ import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.tiem625.beatergdx.Textures
 
-class PlayerActor: Actor() {
+class PlayerActor : Actor() {
 
     val sprite = Sprite(Textures.badLogic).apply {
         this@PlayerActor.width = this.width
         this@PlayerActor.height = this.height
+
+        this@PlayerActor.setOrigin(width / 2, height / 2)
     }
 
     override fun draw(batch: Batch?, parentAlpha: Float) {
--- core/src/com/tiem625/beatergdx/stages/RunStage.kt
+++ core/src/com/tiem625/beatergdx/stages/RunStage.kt
@@ -2,7 +2,6 @@ package com.tiem625.beatergdx.stages
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
-import com.badlogic.gdx.scenes.scene2d.EventListener
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -10,18 +9,24 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.tiem625.beatergdx.actors.PlayerActor
 import com.tiem625.beatergdx.log
 
-class RunStage: Stage(ScreenViewport().apply {
+class RunStage : Stage(ScreenViewport().apply {
 
 }) {
 
     init {
 
-        addListener(object: InputListener() {
+        addListener(object : InputListener() {
 
             override fun keyUp(event: InputEvent?, keycode: Int): Boolean {
                 when (keycode) {
 
                     Input.Keys.ESCAPE -> Gdx.app.exit()
+                    Input.Keys.LEFT -> playerActor.rotateBy(-5.5f)
+                    Input.Keys.RIGHT -> playerActor.rotateBy(5.5f)
+                    Input.Keys.UP -> playerActor.moveBy(5.5f, 0f)
+                    Input.Keys.DOWN -> playerActor.moveBy(-5.5f, 0f)
+
+
                     else -> log("Used keycode: ${keycode}")
                 }
 
